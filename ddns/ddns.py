0d9c2666eb8e7166314f8b7ae554b54fb7bb63b1
Alma-field <66554636+Alma-field@users.noreply.github.com>
2022-03-28 00:49:44 +0900

    不要なメソッドを削除

--- ddns/ddns.py
+++ ddns/ddns.py
@@ -1,5 +1,3 @@
-from requests import get
-
 class DDNS():
     def __init__(self):
         from os import environ
@@ -8,12 +6,3 @@ class DDNS():
 
     def updateip(self):
         raise NotImplementedError('NotImplementedError')
-
-    def get_dnsip(self):
-        if self.hosts[0] == '':
-            domain = self.domain
-        else:
-            domain = f'{self.hosts[0]}.{self.domain}'
-        url = f'https://dns.google.com/resolve?name={domain}&type=A'
-        data = get(url).json()
-        return data['Answer'][0]['data']
