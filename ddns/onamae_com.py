fd750f3fce78f69b6a5aee0028ae78696be0cd27
Alma-field <66554636+Alma-field@users.noreply.github.com>
2022-03-28 00:06:50 +0900

    一つのホストにのみ対応

--- ddns/onamae_com.py
+++ ddns/onamae_com.py
@@ -7,25 +7,24 @@ class OnamaeCom(DDNS):
     def __init__(self):
         super().__init__()
 
-    def updateip(self, ipaddress):
-        for host in self.hosts:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.settimeout(10)
-            s = wrap_socket(sock)
-            s.connect(("ddnsclient.onamae.com", 65010))
-            data = [
-                'LOGIN',
-                f'USERID:{self.user_id}',
-                f'PASSWORD:{self.password}',
-                '.',
-                'MODIP',
-                f'HOSTNAME:{host}',
-                f'DOMNAME:{self.domain}',
-                f'IPV4:{ipaddress}',
-                '.',
-                'LOGOUT',
-                '.'
-            ]
-            for item in data:
-                s.send(item.encode() + b'\r\n')
-            s.close()
+    def updateip(self, host, domain, ipaddress):
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.settimeout(10)
+        s = wrap_socket(sock)
+        s.connect(("ddnsclient.onamae.com", 65010))
+        data = [
+            'LOGIN',
+            f'USERID:{self.user_id}',
+            f'PASSWORD:{self.password}',
+            '.',
+            'MODIP',
+            f'HOSTNAME:{host}',
+            f'DOMNAME:{domain}',
+            f'IPV4:{ipaddress}',
+            '.',
+            'LOGOUT',
+            '.'
+        ]
+        for item in data:
+            s.send(item.encode() + b'\r\n')
+        s.close()
